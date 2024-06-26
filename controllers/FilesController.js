4a08d856814cdb0d5b47ee6b298e461ef0b35e79
leroysb <leroybuliro@gmail.com>
2024-06-26 16:07:01 +0300

    check fail - 5,6,9,11

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -324,7 +324,7 @@ export default class FilesController {
 
     const queryFilter = {
       _id: ObjectId.isValid(fileId) ? new ObjectId(fileId) : NULL_ID,
-      // userId: userId ? new ObjectId(userId) : NULL_ID,
+      userId: userId ? new ObjectId(userId) : NULL_ID,
     };
 
     const file = await dbClient.db.collection('files').findOne(queryFilter);
