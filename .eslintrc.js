349519c9fa98721779dca0f4eb45642ec1f29902
leroysb <leroybuliro@gmail.com>
2024-07-23 12:32:59 +0300

    modifies eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
+    node: true,
     jest: true,
   },
   extends: [
+    'eslint:recommended',
     'airbnb-base',
     'plugin:jest/all',
   ],
