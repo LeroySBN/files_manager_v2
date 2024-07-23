7bfc0951d1e453149e314079f8410f75a60bffb3
leroysb <leroybuliro@gmail.com>
2024-07-23 12:40:38 +0300

    increasing req body size to accomodate hi-res images

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // Express server
 import express from 'express';
 import router from './routes/index';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(express.json());
+app.use(bodyParser.json({ limit: '800kb' }));
 app.use('/', router);
 
 app.listen(port, () => {
