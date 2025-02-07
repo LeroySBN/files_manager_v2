aaf5d6ea17d797e32ba01ae3afe4a5105fda1d5f
LeroySBN <leroybuliro@gmail.com>
2025-02-07 19:01:50 +0300

    incorporating typescript

---
+++ web/src/globals.d.ts
@@ -0,0 +1,35 @@
+// Recognize all css, scss, jpg, jpeg, png, svg files as module imports.
+declare module "*.css" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.scss" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.jpg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.jpeg" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: string;
+    export default content;
+}
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
